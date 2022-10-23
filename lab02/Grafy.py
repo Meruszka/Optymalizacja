a828e092fa97be687dcf63000217cafba6e518af
Meruszka <merski70@gmail.com>
2022-10-23 13:37:20 +0200

    lab02

--- lab02/Grafy.py
+++ lab02/Grafy.py
@@ -3,35 +3,30 @@ class GrafNieskierowany:
         self.lista_sasiedztwa = {}
         self.lista_krawedzi = []
         self.lista_wierzcholkow = []
-
     def dodaj_wierzcholek(self, wierzcholek):
         self.lista_wierzcholkow.append(wierzcholek)
         self.lista_sasiedztwa[wierzcholek] = []
-
     def dodaj_krawedz(self, wierzcholek1, wierzcholek2):
         self.lista_krawedzi.append((wierzcholek1, wierzcholek2))
         self.lista_sasiedztwa[wierzcholek1].append(wierzcholek2)
         self.lista_sasiedztwa[wierzcholek2].append(wierzcholek1)
-
     def usun_wierzcholek(self, wierzcholek):
         self.lista_wierzcholkow.remove(wierzcholek)
         self.lista_sasiedztwa.pop(wierzcholek)
         for wierzcholek in self.lista_wierzcholkow:
             if wierzcholek in self.lista_sasiedztwa[wierzcholek]:
                 self.lista_sasiedztwa[wierzcholek].remove(wierzcholek)
-
     def usun_krawedz(self, wierzcholek1, wierzcholek2):
         self.lista_krawedzi.remove((wierzcholek1, wierzcholek2))
         self.lista_sasiedztwa[wierzcholek1].remove(wierzcholek2)
         self.lista_sasiedztwa[wierzcholek2].remove(wierzcholek1)
-
     def wypisz(self):
         print("Wierzcholki: ", self.lista_wierzcholkow)
         print("Krawedzie: ", self.lista_krawedzi)
-        for wierzcholek in self.lista_wierzcholkow:
-            print(wierzcholek, " -> ", self.stopien_wierzcholka(wierzcholek))
+        print("Lista sasiedztwa: ", self.lista_sasiedztwa)
         self.wypisz_macierz_sasiedztwa()
-
+    def wypisz_wierzcholki(self):
+        print(self.lista_wierzcholkow)
     def wypisz_macierz_sasiedztwa(self):
         print("Macierz sasiendztwa:")
         macierz = []
@@ -43,13 +38,8 @@ class GrafNieskierowany:
                 else:
                     wiersz.append(0)
             macierz.append(wiersz)
-        for wiersz in macierz:
-            print(wiersz)
-
+        print(macierz)
         return macierz
-
-    def stopien_wierzcholka(self, wierzcholek):
-        return len(self.lista_sasiedztwa[wierzcholek])
 
 class GrafSkierowany:
     def __init__(self) -> None:
@@ -59,52 +49,34 @@ class GrafSkierowany:
     def dodaj_wierzcholek(self, wierzcholek):
         self.lista_wierzcholkow.append(wierzcholek)
         self.lista_sasiedztwa[wierzcholek] = []
-
     def dodaj_krawedz(self, wierzcholek1, wierzcholek2):
         self.lista_krawedzi.append((wierzcholek1, wierzcholek2))
         self.lista_sasiedztwa[wierzcholek1].append(wierzcholek2)
-
     def usun_wierzcholek(self, wierzcholek):
         self.lista_wierzcholkow.remove(wierzcholek)
         self.lista_sasiedztwa.pop(wierzcholek)
         for wierzcholek in self.lista_wierzcholkow:
             if wierzcholek in self.lista_sasiedztwa[wierzcholek]:
                 self.lista_sasiedztwa[wierzcholek].remove(wierzcholek)
-
     def usun_krawedz(self, wierzcholek1, wierzcholek2):
         self.lista_krawedzi.remove((wierzcholek1, wierzcholek2))
         self.lista_sasiedztwa[wierzcholek1].remove(wierzcholek2)
-
     def wypisz(self):
         print("Wierzcholki: ", self.lista_wierzcholkow)
         print("Krawedzie: ", self.lista_krawedzi)
-        for wierzcholek in self.lista_wierzcholkow:
-            print(wierzcholek, " -> ", self.stopien_wierzcholka(wierzcholek), " -> ", self.stopien_wierzcholka_wchodzacy(wierzcholek))
-        self.wypisz_macierz_sasiedztwa()
-
+        print("Lista sasiedztwa: ", self.lista_sasiedztwa)
+        self.wypisz_macierz_sasiedztwa()    
+    def wypisz_wierzcholki(self):
+        print(self.lista_wierzcholkow)
     def wypisz_macierz_sasiedztwa(self):
         print("Macierz sasiendztwa:")
         macierz = []
-        for wierzcholek1 in self.lista_wierzcholkow:
+        for wierzcholek in self.lista_wierzcholkow:
             wiersz = []
             for wierzcholek2 in self.lista_wierzcholkow:
-                if wierzcholek2 in self.lista_sasiedztwa[wierzcholek1]:
+                if wierzcholek2 in self.lista_sasiedztwa[wierzcholek]:
                     wiersz.append(1)
                 else:
                     wiersz.append(0)
             macierz.append(wiersz)
-        
-        for wiersz in macierz:
-            print(wiersz)
-        return macierz
-
-    def stopien_wierzcholka(self, wierzcholek):            
-        return len(self.lista_sasiedztwa[wierzcholek])
-
-    def stopien_wierzcholka_wchodzacy(self, wierzcholek):
-        stopien = 0
-        for wierzcholek1 in self.lista_wierzcholkow:
-            if wierzcholek in self.lista_sasiedztwa[wierzcholek1]:
-                stopien += 1
-        return stopien
-
+        print(macierz)
---
+++ lab02/zad2.py
@@ -0,0 +1,62 @@
+from Grafy import *
+
+graf = GrafNieskierowany()
+graf.dodaj_wierzcholek(1)
+graf.dodaj_wierzcholek(2)
+graf.dodaj_wierzcholek(3)
+graf.dodaj_wierzcholek(4)
+graf.dodaj_wierzcholek(5)
+graf.dodaj_krawedz(1, 2)
+graf.dodaj_krawedz(2, 3)
+graf.dodaj_krawedz(3, 4)
+graf.dodaj_krawedz(4, 5)
+graf.dodaj_krawedz(5, 1)
+graf.dodaj_krawedz(1, 4)
+
+g = [
+    [0, 1, 0, 1, 1], 
+    [1, 0, 1, 0, 0], 
+    [0, 1, 0, 1, 0], 
+    [1, 0, 1, 0, 1], 
+    [1, 0, 0, 1, 0]]
+
+
+def znajdz_cykl_c3_macierz(g):
+    for w1 in range(len(g)):
+        for w2 in range(len(g)):
+            if w1 == w2:
+                continue
+            for w3 in range(len(g)):
+                if w3 == w1 or w3 == w2:
+                    continue
+                if g[w1][w2] > 0 and g[w2][w3] > 0 and g[w3][w1] > 0:
+                    return True
+    return False
+
+print(znajdz_cykl_c3_macierz(g))
+
+def podniesc_macierz_do_potegi(g, n):
+    if n == 0:
+        return g
+    if n == 1:
+        return g
+    return pomnoz_macierz(g, podniesc_macierz_do_potegi(g, n-1))
+
+def pomnoz_macierz(m1, m2):
+    wynik = [[0 for _ in range(len(m1))] for _ in range(len(m1))]
+    for i in range(len(m1)):
+        for j in range(len(m1)):
+            for k in range(len(m1)):
+                wynik[i][j] += m1[i][k] * m2[k][j]
+    return wynik
+
+def znajdz_cykl_c3_macierz_potegi(g):
+    potega = podniesc_macierz_do_potegi(g, 3)
+    for wiersz in potega:
+        print(wiersz)
+    tmp = 0
+    for i in range(len(potega)):
+        tmp += potega[i][i]
+    return tmp/6 > 0
+    
+print(znajdz_cykl_c3_macierz_potegi(g))
