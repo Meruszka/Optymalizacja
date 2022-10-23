4f853ad6ab54e11b320b6c1a0df0974317c2756a
Szymon Merski <merski70@gmail.com>
2022-10-23 14:11:37 +0200

    zad3, lab02

--- lab02/zad2.py
+++ lab02/zad2.py
@@ -14,10 +14,10 @@ graf.dodaj_krawedz(5, 1)
 graf.dodaj_krawedz(1, 4)
 
 g = [
-    [0, 1, 0, 1, 1], 
-    [1, 0, 1, 0, 0], 
-    [0, 1, 0, 1, 0], 
-    [1, 0, 1, 0, 1], 
+    [0, 1, 0, 1, 1],
+    [1, 0, 1, 0, 0],
+    [0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 1],
     [1, 0, 0, 1, 0]]
 
 
@@ -30,10 +30,15 @@ def znajdz_cykl_c3_macierz(g):
                 if w3 == w1 or w3 == w2:
                     continue
                 if g[w1][w2] > 0 and g[w2][w3] > 0 and g[w3][w1] > 0:
-                    return True
+                    cykl = [w1, w2, w3]
+                    for w in range(len(cykl)):
+                        cykl[w] += 1
+                    return True, cykl
     return False
 
+
 print(znajdz_cykl_c3_macierz(g))
+
 
 def podniesc_macierz_do_potegi(g, n):
     if n == 0:
@@ -41,6 +46,7 @@ def podniesc_macierz_do_potegi(g, n):
     if n == 1:
         return g
     return pomnoz_macierz(g, podniesc_macierz_do_potegi(g, n-1))
+
 
 def pomnoz_macierz(m1, m2):
     wynik = [[0 for _ in range(len(m1))] for _ in range(len(m1))]
@@ -50,6 +56,7 @@ def pomnoz_macierz(m1, m2):
                 wynik[i][j] += m1[i][k] * m2[k][j]
     return wynik
 
+
 def znajdz_cykl_c3_macierz_potegi(g):
     potega = podniesc_macierz_do_potegi(g, 3)
     for wiersz in potega:
@@ -58,5 +65,6 @@ def znajdz_cykl_c3_macierz_potegi(g):
     for i in range(len(potega)):
         tmp += potega[i][i]
     return tmp/6 > 0
-    
+
+
 print(znajdz_cykl_c3_macierz_potegi(g))
